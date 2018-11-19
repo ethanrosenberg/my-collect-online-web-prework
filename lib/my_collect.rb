254528f8bf84fe65f90a63abeb262cb47897e486
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-19 08:48:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   count = 0;
   newcollection = []
   while count < collection.size
-    newcollection << yield collection[count]
+    newcollection << yield(collection[count])
 
     count +=1
   end
